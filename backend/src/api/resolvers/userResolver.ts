59eab7182fc8363cffe48180d7baab43403ab57c
seblundin <seblundin@gmail.com>
2024-02-10 17:51:01 +0200

    Change boilerplate

---
+++ backend/src/api/resolvers/userResolver.ts
@@ -0,0 +1,4 @@
+// TODO: add resolver for user
+// Query: users, userById, checkToken
+// Mutation: login, register, updateUser, deleteUser
+
---
+++ backend/src/interfaces/User.ts
@@ -0,0 +1,8 @@
+interface User {
+  id: string;
+  user_name: string;
+  email: string;
+  password?: string;
+}
+
+export {User};
