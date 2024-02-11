58c91b500478ba5fbe9c208a2dfdf3061e096578
Hanna <hanna.heimonen@metropolia.fi>
2024-02-12 01:25:34 +0200

    Create thread button

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import './App.css'
 
 function App() {
   return (
--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -15,10 +15,23 @@ const Home = () => {
 
   const threadList: Thread[] = [thread, thread2]
 
+  const handleCreateThread = () => {
+    // Implement the logic to create a new thread
+    console.log('Create thread button clicked')
+  }
+
   return (
     <>
-      <h1 className="text-5xl font-sans mb-4">Foorumi</h1>
-      <main className="mt-16 bg-pink-100 text-white p-8 rounded-lg">
+      <h1 className="text-5xl font-sans mb-4 text-center p-4">Foorumi</h1>
+      <main className="mt-16 bg-pink-100 text-white p-8 rounded-lg max-w-5xl mx-auto">
+        <div>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
+            onClick={handleCreateThread}
+          >
+            Create Thread
+          </button>
+        </div>
         <div className="space-y-4">
           {threadList.map((thread) => (
             <div
