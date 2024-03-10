38c45c6a7ae1887d329b9ef508189c39e550ebe8
seblundin <seblundin@gmail.com>
2024-03-10 18:52:21 +0200

    Add user context, modify thread type

---
+++ front/src/services/UserService.ts
@@ -0,0 +1,80 @@
+import LoginResponse from '../interfaces/LoginResponse'
+import RegisterResponse from '../interfaces/RegisterResponse'
+
+const loginUser = async (vars: {
+  credentials: { username: string; password: string }
+}): Promise<LoginResponse | undefined> => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `mutation Login($credentials: Credentials!) {
+          login(credentials: $credentials) {
+            token
+            message
+            user {
+              email
+              username
+              id
+            }
+          }
+        }`,
+        variables: vars,
+      }),
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const data: LoginResponse = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const registerUser = async (vars: {
+  user: {
+    username: string
+    email: string
+    password: string
+  }
+}) => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `mutation Mutation($user: UserInput!) {
+          register(user: $user) {
+            message
+            user {
+              id
+              username
+              email
+            }
+          }
+        }`,
+        variables: vars,
+      }),
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const data: RegisterResponse = await response.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { loginUser, registerUser }
